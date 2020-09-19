e5a7ad4c86070c7bd50fe920312760d40bda4ad7
skaldek <skaldek@mail.ru>
2020-09-19 18:52:17 +0200

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -28,52 +28,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 200,
-              child:
-            Column(children: <Widget> [
-              TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Username',
-                  )),
-              TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  )),
-              RaisedButton(
-                onPressed:
-                  () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => NextPage()));
-                  },
-              child: Text('Butt', style: TextStyle(fontSize: 15)),)
-            ])),
-          ],
+    return new Scaffold(
+        body: new Stack(
+      children: <Widget>[
+        new Container(
+          decoration: new BoxDecoration(
+            image: new DecorationImage(
+              image: new AssetImage("assets/images/home_page.png"),
+              fit: BoxFit.fill,
+            ),
+          ),
         ),
-      )
-    );
+        new Container(
+          alignment: Alignment.bottomRight,
+          padding: EdgeInsets.all(30),
+          child: FlatButton(
+            shape: RoundedRectangleBorder(side: BorderSide(
+                color: Colors.white,
+                width: 1,
+                style: BorderStyle.solid
+            ), borderRadius: BorderRadius.circular(10)),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => NextPage()));
+            },
+            child: Text('Давайте начнём!', style: TextStyle(fontSize: 15, color: Colors.white)),
+          ),
+        )
+      ],
+    ));
   }
 }
 
 class NextPage extends StatefulWidget {
-  NextPage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _NextPage createState() => _NextPage();
 }
@@ -81,8 +70,40 @@ class NextPage extends StatefulWidget {
 class _NextPage extends State<NextPage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('123'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  width: 200,
+                  child: Column(children: <Widget>[
+                    TextField(
+                        decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Username',
+                    )),
+                    TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                        )),
+                    RaisedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => NextPage()));
+                      },
+                      child: Text('Butt', style: TextStyle(fontSize: 15)),
+                    )
+                  ])),
+            ],
+          ),
+        ));
   }
-
 }
