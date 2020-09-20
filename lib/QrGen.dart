c65d1eabd349d4cdf2f30334c614bcf9603b9681
skaldek <skaldek@mail.ru>
2020-09-20 05:16:51 +0200

    Qr

---
+++ lib/QrGen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui';
+import 'dart:io';
+import 'package:flutter/rendering.dart';
+import 'package:path_provider/path_provider.dart';
+
+class QrGen extends StatefulWidget {
+  String _dataString;
+  QrGen(String dataString){
+    _dataString = dataString;
+  }
+
+  @override
+  State<StatefulWidget> createState() => _QrGen(_dataString);
+}
+
+class _QrGen extends State<QrGen> {
+
+  _QrGen(String dataString){
+    _dataString = dataString;
+  }
+
+  GlobalKey globalKey = new GlobalKey();
+  String _dataString;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Code Generator'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: _captureAndSharePng,
+          )
+        ],
+      ),
+      body: _contentWidget(),
+    );
+  }
+
+  Future<void> _captureAndSharePng() async {
+    try {
+      RenderRepaintBoundary boundary = globalKey.currentContext.findRenderObject();
+      var image = await boundary.toImage();
+      ByteData byteData = await image.toByteData(format: ImageByteFormat.png);
+      Uint8List pngBytes = byteData.buffer.asUint8List();
+
+      final tempDir = await getTemporaryDirectory();
+      final file = await new File('${tempDir.path}/image.png').create();
+      await file.writeAsBytes(pngBytes);
+
+      final channel = const MethodChannel('channel:me.alfian.share/share');
+      channel.invokeMethod('shareFile', 'image.png');
+
+    } catch(e) {
+      print(e.toString());
+    }
+  }
+
+  _contentWidget() {
+    final bodyHeight = MediaQuery.of(context).size.height - MediaQuery.of(context).viewInsets.bottom;
+    return  Container(
+      color: const Color(0xFFFFFFFF),
+      child:  Column(
+        children: <Widget>[
+          Expanded(
+            child:  Center(
+              child: RepaintBoundary(
+                key: globalKey,
+                child: QrImage(
+                  data: _dataString,
+                  size: 0.4 * bodyHeight
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:hackathonseptember/QrGen.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -144,7 +145,7 @@ class _Login extends State<Login> {
                                     Navigator.push(
                                         context,
                                         MaterialPageRoute(
-                                            builder: (context) => Admin()));
+                                            builder: (context) => User()));
                                     //     }
                                     // });
                                   },
@@ -189,7 +190,7 @@ class _Login extends State<Login> {
 
 Future<bool> testReq(String login, String pass) async {
   var response = await http.get(
-      'https://hackathon-september.herokuapp.com/login?username=' +
+      'http://167.172.188.11:8080/server-1.0-SNAPSHOT/login?username=' +
           login +
           '&password=' +
           pass);
@@ -365,7 +366,7 @@ class _Admin extends State<Admin> {
 
   Future getApps(String id) async {
     var response = await http.get(
-        'https://hackathon-september.herokuapp.com/application_list?userId=' +
+        'http://167.172.188.11:8080/server-1.0-SNAPSHOT/application_list?userId=' +
             id);
     print(response.body);
     List<dynamic> data = jsonDecode(response.body);
@@ -389,95 +390,161 @@ class _NewApp extends State<NewApp> {
         home: Scaffold(
             resizeToAvoidBottomPadding: false,
             appBar: AppBar(
-              title: Text('Добавить вход',
-                  style: TextStyle(color: Colors.white)),
+              title:
+                  Text('Добавить вход', style: TextStyle(color: Colors.white)),
             ),
             body: Container(
               padding: EdgeInsets.all(10),
-                child: Column(
-                  children: <Widget> [
-                    TextField(
-                      decoration: InputDecoration(
-                        border: UnderlineInputBorder(
-                            borderRadius:
-                            BorderRadius.all(Radius.circular(10))),
-                        labelText: 'Ф.И.О',
-                      ),
-                      onChanged: (text) {},
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      labelText: 'Ф.И.О',
                     ),
-                    SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget> [
-                        Container(
-                          width: 150,
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: UnderlineInputBorder(
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(10))),
-                              labelText: 'Серия',
-                            ),
-                            onChanged: (text) {},
+                    onChanged: (text) {},
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        width: 150,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: UnderlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            labelText: 'Серия',
                           ),
+                          onChanged: (text) {},
                         ),
-                        Container(
-                          width: 150,
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: UnderlineInputBorder(
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(10))),
-                              labelText: 'Номер',
-                            ),
-                            onChanged: (text) {},
+                      ),
+                      Container(
+                        width: 150,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: UnderlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            labelText: 'Номер',
                           ),
+                          onChanged: (text) {},
                         ),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    TextField(
-                      decoration: InputDecoration(
-                        border: UnderlineInputBorder(
-                            borderRadius:
-                            BorderRadius.all(Radius.circular(10))),
-                        labelText: 'Телефон',
                       ),
-                      onChanged: (text) {},
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      labelText: 'Телефон',
                     ),
-                    SizedBox(height: 10),
-                    TextField(
-                      keyboardType: TextInputType.multiline,
-                      maxLines: null,
-                      minLines: 2,
-                      decoration: InputDecoration(
-                        border: UnderlineInputBorder(
-                            borderRadius:
-                            BorderRadius.all(Radius.circular(10))),
-                        labelText: 'О Заявке',
-                      ),
-                      onChanged: (text) {},
+                    onChanged: (text) {},
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    minLines: 2,
+                    decoration: InputDecoration(
+                      border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      labelText: 'О Заявке',
                     ),
-                    SizedBox(height: 50),
-                    Container(
-                      alignment: Alignment.bottomCenter,
-                      child: FlatButton(
-                        shape: RoundedRectangleBorder(
-                            side: BorderSide(
-                                color: Colors.blueAccent,
-                                width: 1,
-                                style: BorderStyle.solid),
-                            borderRadius: BorderRadius.circular(10)),
-                        onPressed: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => Admin()));
-                        },
-                        child: Text('Добавить запись!',
-                            style: TextStyle(fontSize: 18, color: Colors.blueAccent)),
-                      ),
+                    onChanged: (text) {},
+                  ),
+                  SizedBox(height: 50),
+                  Container(
+                    alignment: Alignment.bottomCenter,
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                              color: Colors.blueAccent,
+                              width: 1,
+                              style: BorderStyle.solid),
+                          borderRadius: BorderRadius.circular(10)),
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => QrGen('123')));
+                      },
+                      child: Text('Добавить запись!',
+                          style: TextStyle(
+                              fontSize: 18, color: Colors.blueAccent)),
                     ),
-                  ],
-                ),
+                  ),
+                ],
+              ),
             )));
+  }
+}
+
+class User extends StatefulWidget {
+  @override
+  _User createState() => _User();
+}
+
+class _User extends State<User> {
+  List<String> numberTruthList = new List<String>();
+  List<bool> states = new List<bool>();
+
+  @override
+  initState() {
+    super.initState();
+
+    getApps('abc123').then((value) => setState(() => {}));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          resizeToAvoidBottomPadding: false,
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => NewApp()));
+            },
+          ),
+          appBar: AppBar(
+            title: Text('User', style: TextStyle(color: Colors.white)),
+          ),
+          body: Center(
+              child: ListView.builder(
+                itemCount: numberTruthList.length,
+                itemBuilder: (context, i) {
+                  return ListTile(
+                    title: Text(numberTruthList[i]),
+                    trailing: Icon(
+                      // NEW from here...
+                      states[i] ? Icons.check : Icons.cancel,
+                      color: states[i] ? Colors.green : Colors.red,
+                    ),
+                    onTap: () {
+                      setState(() {
+                        Navigator.push(
+                            context, MaterialPageRoute(builder: (context) => QrGen('123')));
+                      });
+                    },
+                  );
+                },
+              ))),
+    );
+  }
+
+  Future getApps(String id) async {
+    var response = await http.get(
+        'http://167.172.188.11:8080/server-1.0-SNAPSHOT/application_list?userId=' +
+            id);
+    print(response.body);
+    List<dynamic> data = jsonDecode(response.body);
+    print(data.length);
+    for (int i = 0; i < data.length; i++) {
+      numberTruthList.add(data[i]['description']);
+      states.add(false);
+    }
   }
 }
